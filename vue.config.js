3eaa81f7f451bb59c4f2778cf89a306ac4710461
Pelle Koster <pelle.koster@nginfra.nl>
2021-11-16 12:03:50 +0100

    add ci config

--- vue.config.js
+++ vue.config.js
@@ -14,8 +14,5 @@ module.exports = {
         additionalData: `@import "./src/assets/sass/variables.scss";`
       }
     }
-  },
-  configureWebpack: config => {
-    config.resolve.alias['@movici-flow-common'] = path.join(__dirname, 'movici-flow-common/src/');
   }
 };
