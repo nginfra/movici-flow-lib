ea251dac024e4d8aa288b3a71b5a14fa18679a01
Pelle Koster <pelle.koster@nginfra.nl>
2022-01-07 11:35:32 +0100

    some cleanup, which also removes errorneous warnings

--- src/types/backend/geocode.ts
+++
@@ -1,7 +0,0 @@
-import { GeocodeSearchQuery, GeocodeSearchResult, GeocodeSuggestion } from '..';
-
-export default interface GeocodeService {
-  resolveSuggestion(suggestion: GeocodeSuggestion): Promise<GeocodeSearchResult | null>;
-  getSuggestions(query: GeocodeSearchQuery): Promise<GeocodeSuggestion[] | null>;
-  getResults(query: GeocodeSearchQuery): Promise<GeocodeSearchResult[] | null>;
-}
--- src/types/backend/view.ts
+++
@@ -1,9 +0,0 @@
-import { UUID, View, ViewCrudResponse } from '..';
-
-export default interface ViewService {
-  create(scenarioUUID: UUID, view: View): Promise<ViewCrudResponse | null>;
-  list(scenarioUUID: UUID): Promise<View[] | null>;
-  get(viewUUID: UUID): Promise<View | null>;
-  update(viewUUID: UUID, view: View): Promise<ViewCrudResponse | null>;
-  delete(viewUUID: UUID): Promise<ViewCrudResponse | null>;
-}
